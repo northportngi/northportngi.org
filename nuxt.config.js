dc971970726ad8880224740e47feca59b6d59413
Danny Valle <dannyvalle@me.com>
2024-11-21 18:47:00 -0500

    Add Nuxt Studio module.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ['@/assets/css/main.scss'],
+				css: ['@/assets/css/main.scss'],
 
-	modules: ['@nuxt/image', 'nuxt-viewport', '@nuxt/fonts', '@nuxt/content', '@nuxtjs/seo'],
+				modules: [
+				 '@nuxt/image',
+				 'nuxt-viewport',
+				 '@nuxt/fonts',
+				 '@nuxt/content',
+				 '@nuxtjs/seo',
+				 '@nuxthq/studio',
+				],
 
-	content: {
-		markdown: {
-			anchorLinks: false,
-		},
-	},
-
-	app: {
-		head: {
-			htmlAttrs: {
-				lang: 'en',
-			},
-			script: [
-				{
-					src: '/js/accessible-menu.js',
-					body: true,
+				content: {
+								markdown: {
+												anchorLinks: false,
+								},
 				},
-			],
-		},
-	},
 
-	compatibilityDate: '2024-08-05',
+				app: {
+								head: {
+												htmlAttrs: {
+																lang: 'en',
+												},
+												script: [
+																{
+																				src: '/js/accessible-menu.js',
+																				body: true,
+																},
+												],
+								},
+				},
+
+				compatibilityDate: '2024-08-05',
 })
