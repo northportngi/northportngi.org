8435fd0455c19308ea0d150a726621e62ae5f89f
Danny Valle <dannyvalle@me.com>
2025-02-03 23:23:09 -0500

    Add SEO data to nuxt config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	css: ['@/assets/css/main.scss'],
+	site: {
+		url: 'https://northportngi.org',
+		name: 'Northport Native Garden Initiative',
+		description: 'We are a small 501(c)(3) in Northport, NY, empowering our community with tools and knowledge to promote native landscaping for a healthier local ecosystem.',
+		defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
+	},
 
 	modules: [
 		'@nuxt/image',
